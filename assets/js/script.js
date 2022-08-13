a2094955204aed4913478cea858785793e628b3f
Egg <adrianjimenez1950@gmail.com>
2022-08-13 01:08:36 -0400

    finalized styles and animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,16 +26,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    //console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    //console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    //console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    //console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
 
@@ -74,12 +76,13 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    //console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    //console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -179,11 +182,14 @@ $(".list-group").on("click","span", function() {
 });
 
 var auditTask = function(taskEl) {
+
+  console.log(taskEl);
+
   var date = $(taskEl).find("span").text().trim();
-  console.log(date);
+  //console.log(date);
 
   var time = moment(date, "L").set("hour", 17);
-  console.log(time);
+  //console.log(time);
 
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
 
@@ -237,7 +243,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -266,6 +272,14 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+setInterval(function() {
+
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
